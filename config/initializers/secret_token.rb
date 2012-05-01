f02e39ecec1ea1d4efc99ab7525db3b785c70de0
Xing Zhou <dekom.dev@gmail.com>
2012-04-30 21:09:53 -0400

    Initial Commit w/ Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marginalia::Application.config.secret_token = 'f1e99072bc0fb291859a42da6c5aedb5714fa52ef31813991a12a78f2740554750e7fad71afa7559879d1a0e48ef15c1e465f0654e891d177e73dfe195439fd7'
